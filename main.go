eb50707bd76c9244696926cb1e0b403776ba6ecf
emattiza <emattiza@gmail.com>
2022-09-15 10:39:19 -0500

    add basic json schema runner

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+  "encoding/json"
+  "github.com/invopop/jsonschema"
+  talosTypes "github.com/talos-systems/talos/pkg/machinery/config/types/v1alpha1"
+)
+
+
+func main() {
+  s := jsonschema.Reflect(&talosTypes.Config{})
+  data, _ := json.MarshalIndent(s, "", "  ")
+  fmt.Println(string(data))
+
+}
